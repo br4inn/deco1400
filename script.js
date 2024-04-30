5990211c72ba75e4b397e3b96edd688256c8b6c6
Br4inn <jtath04@gmail.com>
2024-04-30 15:19:06 +1000

    s

---
+++ script.js
@@ -0,0 +1,11 @@
+document.querySelector('.aw11').addEventListener('click', function() {
+    window.location.href = 'first_generation.html';
+});
+
+document.querySelector('.sw20').addEventListener('click', function() {
+    window.location.href = 'second_generation.html';
+});
+
+document.querySelector('.mrs').addEventListener('click', function() {
+    window.location.href = 'third_generation.html';
+});
