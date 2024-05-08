3d3133d23144d1f59ddfe57ffc2d49defbfc35dc
Jeremy Tatham <jeremy.tatham@student.uq.edu.au>
2024-05-08 16:56:38 +1000

    weday

--- script.js
+++ script.js
@@ -1,17 +1,6 @@
-document.querySelector('.aw11').addEventListener('click', function() {
-    window.location.href = 'first_generation.html';
-});
+ 
 
-document.querySelector('.sw20').addEventListener('click', function() {
-    window.location.href = 'second_generation.html';
-});
-
-document.querySelector('.mrs').addEventListener('click', function() {
-    window.location.href = 'third_generation.html';
-});
-
-
-
+// Found on w3schools
 let slideIndex = 0;
 showSlides();
 
